3f48af905833c9a06ddf74cf89bc96ec8c11c945
ParadiseV <tangboran1992916@gmail.com>
2018-04-02 20:36:58 +0800

    update something
    function update on April 2th

--- yandereCrawler.py
+++ yandereCrawler.py
@@ -4,15 +4,19 @@ import multiprocessing
 
 
 def mainfunc():
-    x = int(input('输入页数:'))
+    print('您的CPU核心数为：{0}\n本程序将启用{0}个异步进程抓取图片'.format(multiprocessing.cpu_count()))
+    xy = pageinput()
+    x = xy[0]
+    y = xy[1]
     print('正在读取所有链接...')
-    imgpagelists = pagerunner(x)
+    p = multiprocessing.Pool()
+    imgpagelists = p.map(showlink_crawler, range(x, y))
     imgpagelist = listextender(imgpagelists)
     print('已成功读取所有图片页面链接：\n', imgpagelist)
     savepath = input('输入保存路径开始下载:')+'\\'
-    p = multiprocessing.Pool(processes=6)
+    custom(savepath)
     for imglink in imgpagelist:
-        p.apply_async(downloader, (savepath, imglink))
+        p.apply_async(downloader, (savepath, imglink,))
     p.close()
     p.join()
     print('下载已全部完成！')
@@ -23,23 +27,60 @@ def mainfunc():
         exit()
 
 
+def pageinput():
+    try:
+        x = input('输入你希望下载的起始页码(默认为1):')
+        if x == '':
+            x = 1
+        else:
+            x = int(x)
+        y = input('输入你希望下载的最终页码:')
+        y = int(y) + 1
+        if x != 0 and x < y:
+            return x, y
+        else:
+            print('您的输入有误，请输入半角数字，并确保起始页码不为零且小于最终页码！')
+            pageinput()
+    except ValueError:
+        print('您的输入有误，请输入半角数字，并确保起始页码不为零且小于最终页码！')
+        pageinput()
+
+
+def custom(savepath):
+    usercus = input(
+        '在开始下载前是否需要清空log文件？如果您首次使用可任意选择，否则可能导致重复下载。\n'
+        '请输入y或n进行选择(y:是，n:否):')
+    if usercus == 'y':
+        with open(savepath + 'log.json', 'w', encoding='utf-8')as logtext:
+            logtext.writelines('*********************Download Log*********************\n')
+    else:
+        pass
+
+
 def downloader(savepath, imglink):
     link = link_resolver(imglink).get('href')
-    filepath = savepath + imglink.strip('https://yande.re/post/show/') + link[-4:]
-    file_saver(link, filepath)
-
-
-def pagerunner(x):
-    imgpagelists = []
-    for a in range(x):
-        imgpagelists.append(showlink_crawler(a))
-    return imgpagelists
+    imgnm = imglink.strip('https://yande.re/post/show/') + link[-4:]
+    try:
+        with open(savepath + 'log.json', 'r', encoding='utf-8')as logread:
+            logs = logread.readlines()
+        if imgnm + '\n' in logs:
+            print('图片{}已有下载记录，不再下载'.format(imgnm))
+        else:
+            with open(savepath + 'log.json', 'a', encoding='utf-8')as logwrite:
+                logwrite.writelines(imgnm + '\n')
+            filepath = savepath + imgnm
+            file_saver(link, filepath)
+    except FileNotFoundError:
+        with open(savepath + 'log.json', 'w', encoding='utf-8')as logtext:
+            logtext.write('*********************Download Log*********************\n')
+            print('未找到log文件，已在保存路径下创建新的log文件')
+        downloader(savepath, imglink)
 
 
 def listextender(pagelists):
     imgpagelist = []
-    for lists in pagelists:
-        for l in lists:
+    for lis in pagelists:
+        for l in lis:
             imgpagelist.append(l)
     return imgpagelist
 
@@ -68,10 +109,10 @@ def link_resolver(i):
 
 
 def file_saver(link, filepath):
-    print('图片文件' + link.strip('https:'), '正在保存至：' + filepath + '...')
+    print('正在保存图片文件' + link.strip('https:'), '至：' + filepath + '...')
     try:
         with open(filepath):
-            print('当前图片重复，不保存')
+            print('当前图片已存在，不保存')
     except FileNotFoundError:
         pic = requests.get(link, timeout=30)
         with open(filepath, 'wb') as fp:
